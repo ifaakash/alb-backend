f6712f7f431df42ed9bed94a5feb06ad8ca00b75
ifaakash <aakashc.appregis@gmail.com>
2025-08-30 02:02:36 +0530

    chore(Dockerfile): Allow CORS for access from the frontend ( Vite ) application

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import random
 import json
 
 app = FastAPI()
+
+# Allow traffic from the frontend ( to avoid CORS issue )
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        # list of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],          # GET, POST, etc.
+    allow_headers=["*"],          # allow all headers
+)
 
 # Load quotes from a JSON file
 with open("quotes.json") as f:
